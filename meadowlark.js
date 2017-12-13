50ebaf4246776abd35eea44dbdd3c1d54113ba8b
Vladimir Zakharchenko <vmzakharchenko@yandex.ru>
2017-12-13 23:28:23 +0200

    Added routes and templates for 'home', 'about', 'Page - 404' and 'Page - 500'

--- meadowlark.js
+++ meadowlark.js
@@ -10,28 +10,24 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res) {
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	res.render('about');
 });
 
 //Page - 404
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Page no found.');
+	res.render('404');
 });
 
 //Page - 500
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error.');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function() {
